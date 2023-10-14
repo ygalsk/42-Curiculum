3f21cf31449102bb1e8373171bdffc2e258e7000
myls <daniel.kremer@hotmail.de>
2023-10-15 01:12:28 +0200

    changed ft_strdup.c

--- lib/ft_strdup.c
+++ lib/ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: dkremer <dkremer@student.42heilbronn.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/15 00:36:04 by dkremer           #+#    #+#             */
-/*   Updated: 2023/10/15 01:06:33 by dkremer          ###   ########.fr       */
+/*   Updated: 2023/10/15 01:10:55 by dkremer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,13 @@ char	*ft_strdup(const char *s1)
 	char			*dst;
 	const char		*src;
 
+	if (s1 == 0)
+		return (0);
 	len = 0;
 	while (s1[len] != '\0')
 		len++;
 	s2 = (char *)malloc(len + 1);
-	if (s1 == 0 || s2 == 0)
+	if (s2 == 0)
 		return (0);
 	dst = s2;
 	src = s1;
