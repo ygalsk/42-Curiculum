5d210a31fa4bddffd46bd7d64f2c68693b94d9a2
myls <daniel.kremer@hotmail.de>
2023-10-22 16:53:51 +0200

    changed sum

--- lib/ft_memchr.c
+++ lib/ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: dkremer <dkremer@student.42heilbronn.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/14 23:25:35 by dkremer           #+#    #+#             */
-/*   Updated: 2023/10/19 17:30:32 by dkremer          ###   ########.fr       */
+/*   Updated: 2023/10/22 16:52:37 by dkremer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,16 @@
 
 void	*ft_memchr(const void *s, int c, unsigned int n)
 {
-	unsigned char	uc;
-	unsigned char	*p;
+	unsigned char	un_c;
+	unsigned char	*p_s;
 
-	uc = (unsigned char)c;
-	p = (unsigned char *)s;
+	un_c = (unsigned char)c;
+	p_s = (unsigned char *)s;
 	while (n > 0)
 	{
-		if (*p == uc)
-			return ((void *)p);
-		p++;
+		if (*p_s == un_c)
+			return ((void *)p_s);
+		p_s++;
 		n--;
 	}
 	return (0);
--- lib/ft_putendl_fd.c
+++ lib/ft_putendl_fd.c
@@ -6,7 +6,7 @@
 /*   By: dkremer <dkremer@student.42heilbronn.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/22 13:07:37 by dkremer           #+#    #+#             */
-/*   Updated: 2023/10/22 13:22:16 by dkremer          ###   ########.fr       */
+/*   Updated: 2023/10/22 16:17:52 by dkremer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,6 @@ void	ft_putendl_fd(char *s, int fd)
 	if (s != NULL)
 	{
 		write(fd, s, ft_strlen(s));
-		write(fd, '\n', 1);
+		write(fd, "\n", 1);
 	}
 }
